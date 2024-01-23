51edb9db3a99b2d35a437865778ce5d88643e362
Eugeni Bykovski <eugeni.bykovski@itexus.com>
2024-01-23 19:23:47 +0100

    fix bug

--- src/shared/ui/logo.tsx
+++ src/shared/ui/logo.tsx
@@ -5,7 +5,7 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={clsx(className, "flex items-center gap-2 text-xl")}>
       <Shield className="w-12 h-12" />
-      Easy Block
+      Blocker Hosts
     </div>
   );
 };
